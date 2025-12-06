c70d1aa9025de36e8a37fc612a13640b08d024a0
KenArsen <kenzhegulov.kgz@gmail.com>
2025-12-06 16:23:52 +0600

    Теперь fastapi запускаем как python файл

--- src/main.py
+++ src/main.py
@@ -1,4 +1,7 @@
 from fastapi import FastAPI
+import uvicorn
+
+from src.core.config import settings
 
 app = FastAPI()
 
@@ -6,3 +9,12 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "src.main:app",
+        host=settings.API_HOST,
+        port=settings.API_PORT,
+        reload=True
+    )
